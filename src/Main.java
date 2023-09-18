2c65e86e94da55f7d55fbcb85beea1dbb1ec2e21
koendaviduk <davidukj@mail.uc.edu>
2023-09-18 15:23:05 -0400

    finish

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int springCost = 5000;
+        int summerCost = 6500;
+        int fallCost = 4532;
+        int winterCost = 5921;
+        int totalCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("Your total cost for the four seasons, " + "spring: " + springCost + ", summer: " + summerCost +
+                ", fall: " + fallCost + ", and winter: " + winterCost + ", is a grand total of: " + totalCost + ".");
+    }
+}
